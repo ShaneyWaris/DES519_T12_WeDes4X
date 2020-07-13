f1631f5227a3736ec0a40c6103f7d598196230de
ShaneyWaris <ShaneyWaris@users.noreply.github.com>
2020-07-13 19:50:02 +0530

    Update Employees_databse.py

--- Employees_databse.py
+++ Employees_databse.py
@@ -1,5 +1,5 @@
-from flask_pymongo import PyMongo
-# import bson
+# from flask_pymongo import PyMongo
+import bson
 # from main import app
 
 # app.config["MONGO_URI"] = "mongodb+srv://test:test@cluster0.cxhd5.mongodb.net/test?retryWrites=true&w=majority"
